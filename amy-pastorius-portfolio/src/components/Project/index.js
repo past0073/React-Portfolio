26eb2228db2e616d168de6dfe78588d9f1c71d6f
AP <past0073@umn.edu>
2021-05-07 23:25:24 -0500

    Moved styling out of file and into style.css.

--- amy-pastorius-portfolio/src/components/Project/index.js
+++ amy-pastorius-portfolio/src/components/Project/index.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "./style.css";
 
 function Project(props) {
     return (
-        <div className="card text-center" style={{width: 16 + 'rem'}}>
+        <div id="project" className="card text-center">
             <img className="card-img-top" src={props.image} alt={props.alt} />
             <div className="card-body">
             <h5 className="card-title">{props.name}</h5>
             <p className="card-text">{props.discription}</p>
-            <a href={props.repo} target="_blank" style={{position: 'absolute', bottom: 10 + 'px', left: 65 + 'px'}} class="btn btn-primary">Repository</a>
-            <a href={props.liveURL} target="_blank" style={{position: 'absolute', bottom: 50 + 'px', left: 74 + 'px'}} class="btn btn-primary">Live URL</a>
+            <a id="repo" href={props.repo} target="_blank" class="btn btn-primary">Repository</a>
+            <a id="liveURL" href={props.liveURL} target="_blank" class="btn btn-primary">Live URL</a>
             </div>
          </div>
     )
