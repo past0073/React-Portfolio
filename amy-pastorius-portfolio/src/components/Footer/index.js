90f03f5de75ba7f16ff6814ea54c041844495305
AP <past0073@umn.edu>
2021-05-07 20:16:14 -0500

    footer added.

--- amy-pastorius-portfolio/src/components/Footer/index.js
+++ amy-pastorius-portfolio/src/components/Footer/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./style.css";
 
 function Footer () {
     return (
         <div>
-
+        <footer class="footer mt-auto py-3 bg-light">
+            <div className="container">
+                <div>
+                <p>&copy; 2020 Amy Pastorius</p>
+                </div>
+            </div>
+        </footer>
         </div>
     )
 }
--- amy-pastorius-portfolio/src/components/Header/index.js
+++ amy-pastorius-portfolio/src/components/Header/index.js
@@ -3,22 +3,22 @@ import React from "react";
 function Header () {
     return (
         <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="index.html"> Amy Pastorius </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="index.html"> Amy Pastorius </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="/contact">Contact</a>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="/contact">Contact</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/portfolio">Portfolio</a>
+              <li className="nav-item">
+                <a className="nav-link" href="/portfolio">Portfolio</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="/about">About</a>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="/about">About</a>
               </li>
             </ul>
           </div>
