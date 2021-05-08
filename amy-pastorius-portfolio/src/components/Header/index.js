4e91c39f10553e28aee9517680c4b15e5c48f945
AP <past0073@umn.edu>
2021-05-07 20:04:33 -0500

    Header added.

--- amy-pastorius-portfolio/src/components/Header/index.js
+++ amy-pastorius-portfolio/src/components/Header/index.js
@@ -3,7 +3,27 @@ import React from "react";
 function Header () {
     return (
         <div>
-            
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.html"> Amy Pastorius </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="/contact">Contact</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/portfolio">Portfolio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="/about">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
         </div>
     )
 }
