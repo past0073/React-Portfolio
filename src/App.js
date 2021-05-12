887c8b85448cd279516f713f3c3777a8c4bd95da
AP <past0073@umn.edu>
2021-05-12 18:03:35 -0500

    deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {HashRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
